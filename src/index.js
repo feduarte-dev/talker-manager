8d2646bcbb8f700194e277e319ddff259156e639
Felipe Duarte <feduarte.dev@gmail.com>
2023-11-09 14:24:32 -0300

    requisito 3 pronto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require("fs").promises;
 const path = require("path");
+const generateToken = require('./utils/generateToken')
 const app = express();
 app.use(express.json());
 
@@ -39,6 +40,13 @@ app.get("/talker/:id", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+app.post('/login', (req, res) => {
+  const user = { ...req.body };
+  const token = generateToken();
+  res.status(200).json({ token: token });
+
+})
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
