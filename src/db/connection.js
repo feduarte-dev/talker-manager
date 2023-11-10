3ec7ace81aa49311d621f5610f8ed6cdfca3d8d4
Felipe Duarte <feduarte.dev@gmail.com>
2023-11-10 14:27:10 -0300

    refatorando connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'db',
-  port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'TalkerDB',
+  host: 'db',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'TalkerDB',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
